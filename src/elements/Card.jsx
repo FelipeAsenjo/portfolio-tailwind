ec6b6777a3ff491ad1e6e47270221f4110754ef2
Felipe Asenjo <felipe.asenjo2@gmail.com>
2022-11-15 12:30:27 -0300

    video added to section "about me"

--- src/elements/Card.jsx
+++ src/elements/Card.jsx
@@ -4,7 +4,7 @@ export default ({ project, idx }) => {
     return (
         <div className={`max-w-sm w-full my-8 mx-auto rounded-lg shadow-md lg:max-w-fit lg:flex ${isOdd ? 'lg:flex-row-reverse': null}`}>
             <div 
-                className={`h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t-lg lg:rounded-t-none lg:rounded-${isOdd ? 'r':'l'}-lg overflow-hidden`} 
+                className={`h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t-lg lg:rounded-t-none lg:rounded-${!isOdd ? 'l':'r'}-lg overflow-hidden`} 
                 style={{backgroundImage: `url(${ project.imageUrl })`}} 
             />
             <div className="rounded-b-lg bg-white p-4 flex flex-col justify-between lg:rounded-r-lg dark:bg-gray-300">
