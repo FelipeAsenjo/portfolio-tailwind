07a12b419a39f0e6d016476cf7d3a2a753725a8b
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-18 02:11:50 -0400

    Add recommendation data

--- src/context.js
+++ src/context.js
@@ -81,3 +81,38 @@ export const projects = () => {
         },
     ]
 }
+
+export const recommendations = [
+    {
+        recommender: 'Matthew Varga',
+        recommenderPosition: 'Senior Manager',
+        relationship: 'Matthew managed Felipe directly',
+        description: 'Felipe has shown a strong commitment to the tasks and projects he has been assigned to during my time working with him. He works hard to clearly communicate on expectations for his deliverables, delivers clean and well-built solutions, and is able to solve problems creatively. He brings a positive attitude and willingness to learn, and is a great team player.',
+    },
+    {
+        recommender: 'Jordan Jimenez ACC, M.A.',
+        recommenderPosition: 'Professional Coach & HR Manager at Tjene Corp',
+        relationship: 'Jordan worked with Felipe but on different teams',
+        description: `I had the pleasure of hiring Felipe to our Tjene team, I had the opportunity to get to know Felipe during his time here. I have heard positive feedback about both his work and his character. From everything I observed, Felipe brought a strong sense of professionalism, enthusiasm, and integrity to the workplace.
+
+            Felipe approaches challenges with a positive attitude and a willingness to collaborate. Felipe is approachable, thoughtful, and committed to doing his best. While I wasn't part of his immediate team, it was clear through conversations and observations that Felipe made a meaningful impact. He is the kind of person any organization would be lucky to have: enthusiastic, always willing to learn, and a genuinely good teammate.
+
+            I’m confident he will continue to bring great value wherever he goes. Felipe - it was a pleasure working with you, and I hope we cross paths again!
+        `,
+    },
+    {
+        recommender: 'Gabriel Chan',
+        recommenderPosition: 'TRIRIGA Practice Lead at Tjene Corp | IBM Champion',
+        relationship: 'Gabriel managed Felipe directly',
+        description: 'I had the pleasure of working with Felipe on supporting our clients by resolving defects and implementing enhancements in Polymer and React applications. Felipe demonstrated a positive attitude and was always eager to take on new tickets, even when he had no prior experience with similar issues.',
+    },
+    {
+        recommender: 'Ignacio Diaz Ruiz',
+        recommenderPosition: 'Full Stack Developer | ReactJS & NodeJS',
+        relationship: 'Ignacio worked with Felipe on the same team',
+        description: `I had the pleasure of working with Felipe, a highly skilled React developer who consistently impressed me with his dedication, creativity, and clean, scalable code. From building intuitive UI components to optimizing performance, Felipe always brought his A-game and a collaborative spirit to every project.
+
+            He has a deep understanding of React, hooks, state management, and is equally comfortable diving into IBM TRIRIGA when needed. Beyond the code, Felipe is a team player who takes initiative, communicates clearly, and is always open to feedback.
+        `,
+    },
+]
